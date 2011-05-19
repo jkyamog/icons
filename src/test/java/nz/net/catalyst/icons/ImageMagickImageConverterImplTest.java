7ecccbfb729d1176d6784d6885456d0fcca37e8d
Jun Yamog <jun@catalyst.net.nz>
2011-05-19 14:46:21 +1200

    add some crude unit tests

---
+++ src/test/java/nz/net/catalyst/icons/ImageMagickImageConverterImplTest.java
@@ -0,0 +1,56 @@
+package nz.net.catalyst.icons;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+import nz.net.catalyst.icons.ImageConverter.ImageOperation;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImageMagickImageConverterImplTest {
+
+   ImageMagickImageConverterImpl icon;
+   File testDir = new File("target/test-images");
+   
+   @Before
+   public void setup() {
+      icon = new ImageMagickImageConverterImpl();
+      
+      testDir.mkdir();
+   }
+   
+   @After
+   public void cleanup() throws IOException {
+      icon.cleanup();
+
+      FileUtils.deleteDirectory(testDir);
+}
+   
+   @Test
+   public void scaleImage() {
+      File origImage = new File("src/test/resources/Catalyst-IT_Logo.svg");
+      File newImage = new File("target/test-images/catalyst-log.png");
+      
+      try {
+         icon.convert(origImage, newImage, 200, ImageOperation.SCALE);
+
+         ImageInfo ii = icon.getImageInfo(newImage);
+         System.out.println("ImageInfo = " + ii);
+         
+         BufferedImage bi = ImageIO.read(newImage);
+         System.out.println("BufferedImage = " + bi);
+         
+         assertEquals(bi.getWidth(), ii.getWidth());
+      } catch (Exception e) {
+         fail("no exceptions expected");
+      }
+   }
+
+}
